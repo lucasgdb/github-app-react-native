fbb0a3de38bfa4f8f04ed5f0d39e590b4bd33361
lucasnaja <lucasnaja0@gmail.com>
2019-09-26 18:23:21 -0300

    improvements

--- src/pages/UserInformation/index.js
+++ src/pages/UserInformation/index.js
@@ -41,155 +41,153 @@ export default function UserInformation({ navigation }) {
 
   return (
     <View style={{ flex: 1, backgroundColor: '#8A05BE' }}>
+      <View style={{ flexDirection: 'row', padding: 15 }}>
+        <TouchableOpacity onPress={() => Linking.openURL(html_url)}>
+          <Image
+            style={{ width: 60, height: 60 }}
+            source={{ uri: avatar_url }}
+          />
+        </TouchableOpacity>
+
+        <View>
+          <Text style={{ color: '#ededed' }}>
+            {'  '}
+            {name}
+          </Text>
+          <Text style={{ color: '#ccc' }}>
+            {'  '}
+            {login}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ color: '#50046e' }}>
+                {'  '}public repositories:
+              </Text>
+              <Text style={{ color: '#ccc' }}> {public_repos}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ height: 1, backgroundColor: '#ccc' }} />
+
       {loading ? (
         <View style={{ alignItems: 'center', paddingTop: 10 }}>
           <CirclesLoader size={30} color="#ccc" style={{ marginBottom: 5 }} />
         </View>
       ) : (
-        <>
-          <View style={{ flexDirection: 'row', padding: 15 }}>
-            <TouchableOpacity onPress={() => Linking.openURL(html_url)}>
-              <Image
-                style={{ width: 60, height: 60 }}
-                source={{ uri: avatar_url }}
-              />
-            </TouchableOpacity>
-
-            <View>
-              <Text style={{ color: '#ededed' }}>
-                {'  '}
-                {name}
-              </Text>
-              <Text style={{ color: '#ccc' }}>
-                {'  '}
-                {login}
-              </Text>
+        <ScrollView style={{ padding: 15, paddingTop: 5 }}>
+          {repos
+            .filter(repo => !repo.archived && !repo.disabled)
+            .sort((a, b) => b.stargazers_count - a.stargazers_count)
+            .map(repo => (
               <View
                 style={{
-                  flexDirection: 'row',
-                }}>
-                <View style={{ flexDirection: 'row' }}>
-                  <Text style={{ color: '#50046e' }}>
-                    {'  '}public repositories:
-                  </Text>
-                  <Text style={{ color: '#ccc' }}> {public_repos}</Text>
+                  marginBottom: 4,
+                  flex: 1,
+                  borderBottomWidth: 1,
+                  borderColor: '#ccc',
+                  padding: 10,
+                }}
+                key={repo.id}>
+                <TouchableOpacity
+                  onPress={() => Linking.openURL(repo.html_url)}
+                  style={{
+                    flexDirection: 'row',
+                    marginBottom: 6,
+                  }}>
+                  <OctIcon name="repo" size={40} color="#ededed" />
+
+                  <View>
+                    <Text style={{ color: '#ededed' }}>
+                      {'   '}
+                      {repo.full_name}
+                    </Text>
+
+                    <Text style={{ color: '#ccc', fontSize: 12 }}>
+                      {repo.language
+                        ? `   Language: ${repo.language} `
+                        : '   No language '}
+                      ({repo.license ? repo.license.name : 'no license'})
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+
+                <Text
+                  style={{
+                    color: '#ccc',
+                    marginBottom: 10,
+                    borderLeftWidth: repo.description ? 4 : 0,
+                    borderLeftColor: '#50046e',
+                    paddingLeft: repo.description ? 12 : 0,
+                  }}>
+                  {repo.description ? (
+                    <>
+                      "<Bio bio={repo.description} icons={icons} />"
+                    </>
+                  ) : (
+                    'This repo does not have a description.'
+                  )}
+                </Text>
+
+                <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ color: '#50046e' }}>Stars: </Text>
+                    <Text style={{ color: '#ccc' }}>
+                      {repo.stargazers_count}
+                    </Text>
+                  </View>
+
+                  <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                    <Text style={{ color: '#50046e' }}>Forks: </Text>
+                    <Text style={{ color: '#ccc' }}>{repo.forks}</Text>
+                  </View>
+
+                  <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                    <Text style={{ color: '#50046e' }}>Watchers: </Text>
+                    <Text style={{ color: '#ccc' }}>{repo.watchers}</Text>
+                  </View>
                 </View>
-              </View>
-            </View>
-          </View>
 
-          <View style={{ height: 1, backgroundColor: '#ccc' }} />
-
-          <ScrollView style={{ padding: 15, paddingTop: 5 }}>
-            {repos
-              .filter(repo => !repo.archived && !repo.disabled)
-              .sort((a, b) => b.stargazers_count - a.stargazers_count)
-              .map(repo => (
                 <View
                   style={{
-                    marginBottom: 4,
                     flex: 1,
-                    borderBottomWidth: 1,
-                    borderColor: '#ccc',
-                    padding: 10,
-                  }}
-                  key={repo.id}>
-                  <TouchableOpacity
-                    onPress={() => Linking.openURL(repo.html_url)}
-                    style={{
-                      flexDirection: 'row',
-                      marginBottom: 6,
-                    }}>
-                    <OctIcon name="repo" size={40} color="#ededed" />
-
-                    <View>
-                      <Text style={{ color: '#ededed' }}>
-                        {'   '}
-                        {repo.full_name}
-                      </Text>
-
-                      <Text style={{ color: '#ccc', fontSize: 12 }}>
-                        {repo.language
-                          ? `   Language: ${repo.language} `
-                          : '   No language '}
-                        ({repo.license ? repo.license.name : 'no license'})
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-
-                  <Text
-                    style={{
-                      color: '#ccc',
-                      marginBottom: 10,
-                      borderLeftWidth: repo.description ? 4 : 0,
-                      borderLeftColor: '#50046e',
-                      paddingLeft: repo.description ? 12 : 0,
-                    }}>
-                    {repo.description ? (
-                      <>
-                        "<Bio bio={repo.description} icons={icons} />"
-                      </>
-                    ) : (
-                      'This repository does not have a description.'
-                    )}
-                  </Text>
-
-                  <View style={{ flexDirection: 'row', marginBottom: 10 }}>
-                    <View style={{ flexDirection: 'row' }}>
-                      <Text style={{ color: '#50046e' }}>Stars: </Text>
-                      <Text style={{ color: '#ccc' }}>
-                        {repo.stargazers_count}
-                      </Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', marginLeft: 10 }}>
-                      <Text style={{ color: '#50046e' }}>Forks: </Text>
-                      <Text style={{ color: '#ccc' }}>{repo.forks}</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', marginLeft: 10 }}>
-                      <Text style={{ color: '#50046e' }}>Watchers: </Text>
-                      <Text style={{ color: '#ccc' }}>{repo.watchers}</Text>
-                    </View>
-                  </View>
-
-                  <View
+                    flexDirection: 'row',
+                  }}>
+                  <AntIcon.Button
                     style={{
                       flex: 1,
-                      flexDirection: 'row',
-                    }}>
-                    <AntIcon.Button
-                      style={{
-                        flex: 1,
-                      }}
-                      borderRadius={0}
-                      name="star"
-                      color="yellow"
-                      backgroundColor="#000"
-                      onPress={() =>
-                        Linking.openURL(`${repo.html_url}/stargazers`)
-                      }>
-                      <Text style={{ color: '#ededed' }}>Star</Text>
-                    </AntIcon.Button>
+                    }}
+                    borderRadius={0}
+                    name="star"
+                    color="yellow"
+                    backgroundColor="#000"
+                    onPress={() =>
+                      Linking.openURL(`${repo.html_url}/stargazers`)
+                    }>
+                    <Text style={{ color: '#ededed' }}>Star</Text>
+                  </AntIcon.Button>
 
-                    <AntIcon.Button
-                      style={{
-                        flex: 1,
-                      }}
-                      borderRadius={0}
-                      name="fork"
-                      color="#ccc"
-                      backgroundColor="#50046e"
-                      onPress={() =>
-                        Linking.openURL(`${repo.html_url}/network/members`)
-                      }>
-                      <Text style={{ color: '#ededed' }}>Fork</Text>
-                    </AntIcon.Button>
-                  </View>
+                  <AntIcon.Button
+                    style={{
+                      flex: 1,
+                    }}
+                    borderRadius={0}
+                    name="fork"
+                    color="#ccc"
+                    backgroundColor="#50046e"
+                    onPress={() =>
+                      Linking.openURL(`${repo.html_url}/network/members`)
+                    }>
+                    <Text style={{ color: '#ededed' }}>Fork</Text>
+                  </AntIcon.Button>
                 </View>
-              ))}
-          </ScrollView>
-        </>
+              </View>
+            ))}
+        </ScrollView>
       )}
     </View>
   );
